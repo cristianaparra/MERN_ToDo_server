6968e2a1c989849d65d97cf6d9f5e2d9ca6a3dae
Cristian Parra <cristian.a.parra@gmail.com>
2020-09-27 22:26:38 -0300

    creando servidores de express

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('desde indes.js')
+const express = require('express');
+
+//crear el servidro
+
+const app = express()
+
+// puerto de la app
+const PORT = process.env.PORT || 4000;
+
+
+// definir la pagina principal
+app.get('/',(req, res)=>{
+    res.send('hola mundo')
+})
+
+//arranca la app
+app.listen(PORT, () => {
+console.log(`El servidro esta funcionando en el puerto ${PORT}`)
+});
