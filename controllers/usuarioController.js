6a20e414f5e456d780c3c068384e3d556b661f5b
Cristian Parra <cristian.a.parra@gmail.com>
2020-09-30 00:06:15 -0300

    insertando usuario a BD

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,5 +1,21 @@
+const Usuario = require('../models/Usuario')
+
+exports.crearUsuario = async (req, res) => {
 
 
-exports.crearUsuario = (req, res) =>{
-console.log(req.body);
+    try {
+        let usuario;
+        //crear el nuevo usuario
+        usuario = new Usuario(req.body);
+
+        //guardar usuario
+        await usuario.save();
+
+        //mensaje de confirmacion
+        res.send('usuario creado correcamente')
+
+    } catch (error) {
+        console.log('error')
+        res.status(400).send('algo paso')
+    }
 }
