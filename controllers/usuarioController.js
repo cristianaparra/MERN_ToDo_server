b9a3743bbfe906ed978904e82d908a6f5c73fff0
Cristian Parra <cristian.a.parra@gmail.com>
2020-10-01 16:26:24 -0300

    hash password

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,18 +1,30 @@
 const Usuario = require('../models/Usuario')
+const bcrypt = require('bcryptjs')
 
 exports.crearUsuario = async (req, res) => {
-
+    //extraer email y password
+    const { email, password } = req.body;
 
     try {
-        let usuario;
+        // revisart que el usuario registrado sea unico
+        let usuario = await Usuario.findOne({ email });
+
+        if (usuario) {
+            return res.status(400).json({ msg: 'el usuareio ya esiste' })
+        }
         //crear el nuevo usuario
         usuario = new Usuario(req.body);
+
+
+        //hashear el password
+        const salt = await bcrypt.genSalt(10);
+        usuario.password = await bcrypt.hash(password, salt );
 
         //guardar usuario
         await usuario.save();
 
         //mensaje de confirmacion
-        res.send('usuario creado correcamente')
+        res.status(400).json({ msg: 'el usuareio estra creado correctamente' })
 
     } catch (error) {
         console.log('error')
